580e31920daf3b124535624e3d6058ce9b77be82
Marcos Santos <marcos.diego.ns@outlook.com>
2024-10-12 09:57:47 -0500

    feat: integrate faker for dynamic question generation
    Add @faker-js/faker dependency to generate realistic test data. Updated makeQuestion factory to use faker for generating title and content fields, enhancing test cases.

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,14 +3,19 @@ import {
   QuestionProps,
 } from "@/domain/forum/enterprise/entities/question";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { faker } from "@faker-js/faker";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: "Example",
-    content: "Example content",
-    ...override,
-  });
-
-  return question;
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  return Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  );
 }
