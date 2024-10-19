52982c8c5b589793709f22146d9407cfb89c6162
Marcos Santos <marcos.diego.ns@outlook.com>
2024-10-19 16:03:07 -0500

    feat: implement question deletion use case
    Add DeleteQuestionUseCase to handle the deletion of questions by their authors. Introduce tests to ensure proper functionality, including author validation and error handling. Update InMemoryQuestionsRepository and QuestionsRepository with necessary methods for this feature.

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,11 +4,20 @@ import { Question } from "@/domain/forum/enterprise/entities/question";
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
 
-  async findBySlug(slug: string): Promise<Question | null> {
+  async findById(id: string) {
+    return this.items.find((question) => question.id.toString() === id) || null;
+  }
+
+  async findBySlug(slug: string) {
     return this.items.find((question) => question.slug.value === slug) || null;
   }
 
   async create(question: Question) {
     this.items.push(question);
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+    this.items.splice(itemIndex, 1);
   }
 }
