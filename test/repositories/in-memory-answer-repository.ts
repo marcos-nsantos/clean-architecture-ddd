c6335054a74897251e2899a5f722633b12348027
Marcos Santos <marcos.diego.ns@outlook.com>
2024-10-19 16:08:29 -0500

    feat: add functionality to delete forum answers
    Implemented `DeleteAnswerUseCase` with authorization checks and tests to support deleting answers in the forum. Updated `AnswersRepository` to include `findById` and `delete` methods, and created helper functions and classes for testing.

--- test/repositories/in-memory-answer-repository.ts
+++ test/repositories/in-memory-answer-repository.ts
@@ -4,7 +4,16 @@ import { Answer } from "@/domain/forum/enterprise/entities/answer";
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = [];
 
+  async findById(id: string): Promise<Answer | null> {
+    return this.items.find((answer) => answer.id.toString() === id) || null;
+  }
+
   async create(answer: Answer) {
     this.items.push(answer);
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id);
+    this.items.splice(itemIndex, 1);
   }
 }
