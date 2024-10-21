16096778729ad9f355ba2c3ddd346bd85c77b012
Marcos Santos <marcos.diego.ns@outlook.com>
2024-10-21 17:05:45 -0500

    feat: implement fetch recent questions use case
    Add FetchRecentQuestionsUseCase with pagination support for fetching recent questions. Introduce `PaginationParams` interface and enhance InMemoryQuestionsRepository to support pagination and sorting by creation date. Added tests to verify fetching recent and paginated questions.

--- src/domain/forum/application/repositories/question-repository.ts
+++ src/domain/forum/application/repositories/question-repository.ts
@@ -1,9 +1,12 @@
 import { Question } from "@/domain/forum/enterprise/entities/question";
+import { PaginationParams } from "@/core/repositories/pagination-params";
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>;
 
   findBySlug(slug: string): Promise<Question | null>;
+
+  findManyRecent(params: PaginationParams): Promise<Question[]>;
 
   save(question: Question): Promise<void>;
 
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,5 +1,6 @@
 import { QuestionsRepository } from "@/domain/forum/application/repositories/question-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
+import { PaginationParams } from "@/core/repositories/pagination-params";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
@@ -10,6 +11,12 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async findBySlug(slug: string) {
     return this.items.find((question) => question.slug.value === slug) || null;
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    return this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20);
   }
 
   async save(question: Question) {
