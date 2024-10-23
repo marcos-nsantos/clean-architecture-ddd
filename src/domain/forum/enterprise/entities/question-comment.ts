547c984242267f0283d7a1b2355b8a12db7dd59a
Marcos Santos <marcosdiego.ns01@gmail.com>
2024-10-23 07:06:29 -0500

    refactor: extract common comment logic into base class
    Created a new abstract Comment class to house shared properties and methods for QuestionComment and AnswerComment entities. This refactoring promotes code reusability and simplifies future maintenance.

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,52 +1,27 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Optional } from "@/core/types/optional";
 import { Entity } from "@/core/entities/entitiy";
+import { CommentProps } from "@/domain/forum/enterprise/entities/comment";
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID;
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID;
-  content: string;
-  createdAt: Date;
-  updatedAt?: Date;
 }
 
 export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId;
-  }
-
-  get content() {
-    return this.props.content;
-  }
-
-  get createdAt() {
-    return this.props.createdAt;
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date();
-  }
-
-  set content(content: string) {
-    this.props.content = content;
-    this.touch();
+  get questionId() {
+    return this.props.questionId;
   }
 
   static create(
     props: Optional<QuestionCommentProps, "createdAt">,
     id?: UniqueEntityID,
   ) {
-    const questionComment = new QuestionComment(
+    return new QuestionComment(
       {
         ...props,
         createdAt: props.createdAt ?? new Date(),
       },
       id,
     );
-    return questionComment;
   }
 }
