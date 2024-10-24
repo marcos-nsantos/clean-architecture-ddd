c2ffb86d3c1fd9edeb001594b283bf6a956f9517
Marcos Santos <marcos.diego.ns@outlook.com>
2024-10-24 18:47:58 -0500

    feat: add fetching of paginated answer comments
    Introduced use case and its implementation to fetch paginated answer comments. Added necessary repository methods and tests to support this functionality.

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,5 +1,6 @@
 import { AnswerCommentsRepository } from "../../src/domain/forum/application/repositories/answer-comment-repository";
 import { AnswerComment } from "../../src/domain/forum/enterprise/entities/answer-comment";
+import { PaginationParams } from "@/core/repositories/pagination-params";
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
@@ -9,6 +10,12 @@ export class InMemoryAnswerCommentsRepository
   async findById(id: string) {
     const answerComment = this.items.find((item) => item.id.toString() === id);
     return answerComment || null;
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    return this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20);
   }
 
   async create(answerComment: AnswerComment) {
